39017c584104066ff8dc5b9066e0d7274abcf605
NadyaEvp <t_evp@mail.ru>
2024-08-06 17:41:00 +0300

    Чиним названия переменных

--- hh.py
+++ hh.py
@@ -7,7 +7,7 @@ def predict_rub_salary_for_hh(vacancy_info):
     
 
 def parse_hh_vacancies(languages):
-  url_hh = 'https://api.hh.ru/vacancies'
+  hh_url = 'https://api.hh.ru/vacancies'
   param_settings = {'index_moscov': 1, 'public_period': 30, 'items_page': 100}
   params_hh = {
       'text': '',
@@ -16,21 +16,18 @@ def parse_hh_vacancies(languages):
       'per_page': param_settings['items_page'],
       'page': 0,
   }
-  language_info = {
         'vacancies_found': 0,
         'vacancies_processed': 0,
         'average_salary': 0,
   }
-  hh_pages = 1
-  all_vacancies = []
 
   for language_key in languages:
     params_hh['text'] = f'программист {language_key}'
     while params_hh['page'] < hh_pages:
-      response = requests.get(url_hh, params=params_hh)
+      response = requests.get(hh_url, params=params_hh)
       response.raise_for_status()
       hh_response = response.json()
-      language_info['vacancies_found'] = hh_response['found']
+      language_section['vacancies_found'] = hh_response['found']
       all_vacancies += hh_response['items']
       hh_pages = hh_response['pages']
       params_hh['page'] += 1
@@ -38,8 +35,11 @@ def parse_hh_vacancies(languages):
     for vacancy in all_vacancies:
       job_salary = predict_rub_salary_for_hh(vacancy)
       if job_salary:
-        language_info['vacancies_processed'] += 1
-        language_info['average_salary'] += job_salary
-    language_info['average_salary'] = int(language_info['average_salary'] / language_info['vacancies_processed'])
+        language_section['vacancies_processed'] += 1
+        language_section['average_salary'] += job_salary
+    try:
+      language_section['average_salary'] = int(language_section['average_salary'] / language_section['vacancies_processed'])
+    except ZeroDivisionError:
+       print('Ошибка деления на ноль')
 
-    return language_info
+    return language_section
