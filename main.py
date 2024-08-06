87e829d674a3624f4bf517f077f02c386c4c6e4a
NadyaEvp <t_evp@mail.ru>
2024-08-06 17:41:59 +0300

    Добавляем вывод таблиц

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import requests
 from dotenv import load_dotenv
 from draw_table import make_table
 from general_function import get_statistics
@@ -11,8 +10,10 @@ def main():
     load_dotenv()
     superjob_secret_key = os.getenv('SUPERJOB_SECRET_KEY')
     languages = ['Python', 'Java', 'JavaScript', 'C++', 'C#', 'C', 'Go', 'Scala', 'PHP', 'TypeScript']
-    make_table(' SuperJob Moscow ', get_statistics(parse_superjob_vacancies, languages, superjob_secret_key))
-    make_table(' HeadHunter Moscow ', get_statistics(parse_hh_vacancies, languages))
+    print(make_table(' SuperJob Moscow ', get_statistics(parse_superjob_vacancies, languages, superjob_secret_key)))
+    print()
+    print(make_table(' HeadHunter Moscow ', get_statistics(parse_hh_vacancies, languages)))
+    print()
 
 if __name__ == '__main__':
     main()
