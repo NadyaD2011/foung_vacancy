be744578558eecb14a7c7fa94ed7b59a02b36e5a
NadyaEvp <t_evp@mail.ru>
2024-08-06 17:41:15 +0300

    Чиним названия переменных

--- sj.py
+++ sj.py
@@ -19,21 +19,21 @@ def parse_superjob_vacancies(languages, superjob_secret_key):
       'catalogues': param_settings['index_catalogues'],
       'no_agreement': param_settings['setting_salary'],
   }
-  language_info = {
+  language_section = {
         'vacancies_found': 0,
         'vacancies_processed': 0,
         'average_salary': 0,
-    }
-  url_sj = 'https://api.superjob.ru/2.0/vacancies/'
+  }
+  sj_url = 'https://api.superjob.ru/2.0/vacancies/'
   superjob_pages = True
   all_vacancies = []
   for langeage_key in languages:
     params_sj['keyword'] = langeage_key
     while superjob_pages:
-      response = requests.get(url_sj, headers=headers_sj, params=params_sj)
+      response = requests.get(sj_url, headers=headers_sj, params=params_sj)
       response.raise_for_status()
       superjob_response = response.json()
-      language_info['vacancies_found'] = superjob_response['total']
+      language_section['vacancies_found'] = superjob_response['total']
       all_vacancies += superjob_response['objects']
       superjob_pages = superjob_response['more']
       params_sj['page'] += 1
@@ -41,8 +41,11 @@ def parse_superjob_vacancies(languages, superjob_secret_key):
     for vacancy in all_vacancies:
         job_salary = fetch_rub_salary_for_superJob(vacancy)
         if job_salary:
-          language_info['vacancies_processed'] += 1
-          language_info['average_salary'] += job_salary
-    language_info['average_salary'] = int(language_info['average_salary'] / language_info['vacancies_processed'])
+          language_section['vacancies_processed'] += 1
+          language_section['average_salary'] += job_salary
+    try:      
+      language_section['average_salary'] = int(language_section['average_salary'] / language_section['vacancies_processed'])
+    except ZeroDivisionError:
+       print('Ошибка деления на ноль')
 
-    return language_info
+    return language_section
